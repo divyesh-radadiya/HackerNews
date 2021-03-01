e753d3ee7698b1094aa6b455b31da0fe5e0949c6
divyesh26 <divyesh.r@solutelabs.com>
2021-03-01 12:19:36 +0530

    add some refector

--- hacker_news/lib/services/network_service.dart
+++ hacker_news/lib/services/network_service.dart
@@ -1,15 +1,12 @@
 import 'package:dio/dio.dart';
+import 'package:hacker_news/view_model/news_model.dart';
 
 class NetworkService {
-  NetworkService(this._url);
-
-  final String _url;
-
-  Future<dynamic> getData() async {
+  Future<dynamic> _getData(String url) async {
     try {
       final Dio dio = Dio();
-      dio.options.connectTimeout = 5000;
-      final Response<dynamic> response = await dio.get(_url);
+      dio.options.connectTimeout = 10000;
+      final Response<dynamic> response = await dio.get(url);
       return response.data;
     } on DioError catch (e) {
       if (e.response != null) {
@@ -20,6 +17,30 @@ class NetworkService {
         print(e.request);
         print(e.message);
       }
+      return null;
+    }
+  }
+
+  Future<List<int>> getTopIds() async {
+    List<int> ids = <int>[];
+    final dynamic allIds = await _getData(
+        'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+    if (allIds != null) {
+      for (final dynamic x in allIds) {
+        ids.add(x as int);
+      }
+    }
+    return ids;
+  }
+
+  Future<NewsModel> getNewsById(int id) async {
+    final String url =
+        'https://hacker-news.firebaseio.com/v0/item/$id.json?print=pretty';
+
+    final dynamic news = await _getData(url);
+    if (news != null) {
+      return NewsModel.fromJson(news as Map<String, dynamic>);
+    } else {
       return null;
     }
   }
--- hacker_news/lib/view_model/bloc/hacker_news_bloc.dart
+++ hacker_news/lib/view_model/bloc/hacker_news_bloc.dart
@@ -14,77 +14,51 @@ class HackerNewsBloc extends Bloc<HackerNewsEvent, HackerNewsState> {
   List<NewsModel> newsList = <NewsModel>[];
   List<int> allTopIds = <int>[];
   List<NewsTableData> allDataFromTable = <NewsTableData>[];
-  int index = 0;
   final AppDatabase _appDatabase = AppDatabase();
+  final NetworkService _networkService = NetworkService();
 
   @override
   Stream<HackerNewsState> mapEventToState(
     HackerNewsEvent event,
   ) async* {
     if (event is GetData) {
-      if (allTopIds.isEmpty) await getTopIds();
+      if (allTopIds.isEmpty) await _networkService.getTopIds();
       if (newsList.isEmpty) {
         yield LoadingState();
       }
       final List<NewsModel> newNewsList = <NewsModel>[];
       if (allTopIds.isNotEmpty) {
-        for (int i = 0; i < 10 && index < 500; i++) {
+        for (int i = 0; i < 10 && newsList.length < allTopIds.length; i++) {
           final NewsTableData data =
-              await _appDatabase.getNewsById(allTopIds[index]);
+              await _appDatabase.getNewsById(allTopIds[newsList.length]);
 
           if (data != null) {
             newNewsList.add(NewsModel.fromJson(data.toJson()));
-            index++;
           } else {
-            final NewsModel newNews = await getNewsById(allTopIds[index]);
+            final NewsModel newNews =
+                await _networkService.getNewsById(allTopIds[newsList.length]);
             if (newNews != null) {
               newNewsList.add(newNews);
               await _appDatabase
                   .insertNews(NewsTableData.fromJson(newNews.toJson()));
-              index++;
             }
           }
         }
       } else {
-        if (allDataFromTable.isEmpty) await getFromTable();
-        for (int i = 0; i < 10; i++, index++) {
-          if (allDataFromTable.length > index) {
-            newNewsList
-                .add(NewsModel.fromJson(allDataFromTable[index].toJson()));
+        if (allDataFromTable.isEmpty) {
+          allDataFromTable = await _appDatabase.getAllNews();
+        }
+
+        for (int i = 0; i < 10; i++) {
+          if (allDataFromTable.length > newsList.length) {
+            newNewsList.add(
+                NewsModel.fromJson(allDataFromTable[newsList.length].toJson()));
           }
         }
       }
       newsList.addAll(newNewsList);
 
       yield SuccessState(newsList: newsList);
-    }
-  }
-
-  Future<void> getTopIds() async {
-    final NetworkService networkService = NetworkService(
-        'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
-
-    final dynamic allIds = await networkService.getData();
-    if (allIds != null) {
-      for (final dynamic x in allIds) {
-        allTopIds.add(x as int);
-      }
-    }
-  }
-
-  Future<void> getFromTable() async {
-    allDataFromTable = await (_appDatabase.getAllNews());
-  }
-
-  Future<NewsModel> getNewsById(int id) async {
-    final NetworkService networkService = NetworkService(
-        'https://hacker-news.firebaseio.com/v0/item/$id.json?print=pretty');
-
-    final dynamic news = await networkService.getData();
-    if (news != null) {
-      return NewsModel.fromJson(news as Map<String, dynamic>);
-    } else {
-      return null;
     }
   }
 }
